c1b81a8823a4d2f6473f10c9d930af29c37a73af
Shaiksadaf17 <shaiksadafpatel786@gmail.com>
2024-01-25 01:16:11 +0530

    Update ViewController.swift

--- Qrcode Generator/ViewController.swift
+++ Qrcode Generator/ViewController.swift
@@ -9,14 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var qrImageView: UIImageView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print("hello")
-        print("sadadf")
-        // Do any additional setup after loading the view.
     }
 
 
-}
+    @IBAction func generateAction(_ sender: Any) {
+        if let myName = nameText.text, let qrCodeImage = generateQRCode(urlString: myName) {
+                    qrImageView.image = qrCodeImage
+                }
+            }
 
+            func generateQRCode(urlString: String) -> UIImage? {
+                if let url = URL(string: urlString) {
+                    if let filter = CIFilter(name: "CIQRCodeGenerator") {
+                        let data = url.absoluteString.data(using: .utf8)
+                        filter.setValue(data, forKey: "inputMessage")
+                        let transform = CGAffineTransform(scaleX: 3, y: 3)
+                        
+                        if let output = filter.outputImage?.transformed(by: transform) {
+                            return UIImage(ciImage: output)
+                        }
+                    }
+                }
+                return nil
+            }
+        }
